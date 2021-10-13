45f6b3fd0ec259ab63d6c4423b3541802c1eab9b
mete <mttrkgl1907@gmail.com>
2021-10-13 15:39:32 -0400

    flask-04-add

--- python/hands-on/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods_2/app.py
+++ python/hands-on/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods_2/app.py
@@ -32,4 +32,5 @@ def calc():
 
 # Add a statement to run the Flask application which can be debugged.
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
